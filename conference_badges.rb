d78d9a9cec07069cc38c2de53bcc8a546e081fed
Ebony Sharrock <ebonysharrock@gmail.com>
2019-11-07 09:55:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker(name)
+  p "Hello, my name is #{name}."
+end
+def batch_badge_creator(names)
+  badges=[]
+  names.each {|name| badges << "Hello, my name is #{name}."}
+  badges
+end
+def assign_rooms(members)
+  assignments=[]
+  counter= 0
+  while counter < members.size do
+  assignments << "Hello, #{members[counter]}! You'll be assigned to room #{counter+1}!"
+  counter +=1
+  end
+  assignments
+end
+def printer(list)
+  list.each {|person| p "#{badges(person)} #{assignments(person)}"}
+end
